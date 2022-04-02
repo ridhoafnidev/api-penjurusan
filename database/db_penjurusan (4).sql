4f43c44b7f4ff933e654d3e06f8e86d4e01eac19
ifvodeky24 <ifvodeky.w24@gmail.com>
2022-04-02 21:53:55 +0700

    Changes database

--- database/db_penjurusan (4).sql
+++ database/db_penjurusan (4).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2022 at 09:11 AM
+-- Generation Time: Apr 02, 2022 at 04:52 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.28
 
@@ -45,7 +45,12 @@ CREATE TABLE `tb_guru` (
 --
 
 INSERT INTO `tb_guru` (`id`, `user_id`, `nama`, `username`, `nip`, `alamat`, `foto`, `email`, `created_at`, `updated_at`) VALUES
-(1, 2, 'Guru Andara', 'guru', 124345456, 'Jalan Karya No 5', '', 'guru@gmail.com', '2022-03-26 02:47:09', NULL);
+(1, 2, 'Guru Kuy', 'guru', 2222222, 'uiuiu hjhuh', '124345456_images.jpeg', 'gurukuy@gmail.com', '2022-03-26 02:47:09', '2022-03-31 16:23:14'),
+(2, 9, 'Rila', 'rila c', 11223344, 'Jalans dsdksd', '', 'rila@gmail.com', '2022-03-26 14:58:19', '2022-03-26 14:58:19'),
+(3, 14, 'Rila', 'rilab', 11223344, 'Jalans dsdksd', '', 'rila@gmail.com', '2022-03-27 15:38:15', '2022-03-27 15:38:15'),
+(4, 16, 'helo', 'helo', 122434545, 'helo', '', 'helo@gmail.com', '2022-04-02 07:56:03', '2022-04-02 07:56:03'),
+(5, 17, 'hoi', 'hoi', 12323434, 'gfgfg', '', 'gfgfg@gmail.com', '2022-04-02 08:03:20', '2022-04-02 08:03:20'),
+(6, 18, 'ghghg', 'tes', 545476, 'hjhjjh', '', 'ghghghg', '2022-04-02 08:12:53', '2022-04-02 08:12:53');
 
 -- --------------------------------------------------------
 
@@ -163,7 +168,8 @@ CREATE TABLE `tb_siswa` (
   `pekerjaan_ayah` varchar(50) NOT NULL,
   `nama_ibu` varchar(50) NOT NULL,
   `umur_ibu` varchar(50) NOT NULL,
-  `pendidikan_ibu` varchar(50) NOT NULL,
+  `agama_ibu` varchar(50) NOT NULL,
+  `pendidikan_terakhir_ibu` varchar(50) NOT NULL,
   `pekerjaan_ibu` varchar(50) NOT NULL,
   `tempat_lahir` varchar(50) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
@@ -174,8 +180,11 @@ CREATE TABLE `tb_siswa` (
 -- Dumping data for table `tb_siswa`
 --
 
-INSERT INTO `tb_siswa` (`id`, `user_id`, `username`, `nisn`, `nama`, `kelas`, `tanggal_lahir`, `agama`, `alamat`, `foto`, `asal_sekolah`, `status_asal_sekolah`, `nama_ayah`, `umur_ayah`, `agama_ayah`, `pendidikan_terakhir_ayah`, `pekerjaan_ayah`, `nama_ibu`, `umur_ibu`, `pendidikan_ibu`, `pekerjaan_ibu`, `tempat_lahir`, `created_at`, `updated_at`) VALUES
-(1, 1, 'admin', '12345', 'Budi Waluyo', 'V', '2022-02-04', 'Islam', 'Pekanbaru', '', 'MAN 1 Model Pekanbaru', 'Swasta', 'Johansyah', '40', 'Islam', 'S1', 'Pegawai Bank', 'Faridah', '35', 'S2', 'DOSEN', 'Pekanbaru', '2022-03-22 15:08:22', '2022-02-05 13:53:04');
+INSERT INTO `tb_siswa` (`id`, `user_id`, `username`, `nisn`, `nama`, `kelas`, `tanggal_lahir`, `agama`, `alamat`, `foto`, `asal_sekolah`, `status_asal_sekolah`, `nama_ayah`, `umur_ayah`, `agama_ayah`, `pendidikan_terakhir_ayah`, `pekerjaan_ayah`, `nama_ibu`, `umur_ibu`, `agama_ibu`, `pendidikan_terakhir_ibu`, `pekerjaan_ibu`, `tempat_lahir`, `created_at`, `updated_at`) VALUES
+(1, 1, 'admin', '111111111111', 'Murid Hei', 'II IPS', '2022-02-04', 'Islam', 'rengat fgfg', '12345_images.jpeg', 'SMAN 1', 'Negeri', 'Dudung', '49', 'Islam', 'D3/S1', 'Guru', 'Walia', '45', 'Islam', 'D3/S1', 'Guru', 'Rengat', '2022-03-30 17:08:14', '2022-03-30 17:08:14'),
+(2, 13, 'ridho', '123224343', 'Ridho Afni', 'II IPS', '2022-02-04', 'Islam', 'Jalan negara', '', 'SMAN 1', 'Negeri', 'Ayah', '12', 'Islam', 'D3/S1', 'Karyawan', 'Rohina', '40', 'Islam', 'D3/S1', 'Karyawan', 'riau', '2022-03-30 13:47:33', '2022-03-26 15:08:52'),
+(3, 15, 'ridhos', '123224343', 'Ridho Afni', 'II IPS', '2022-02-04', 'Islam', 'Jalan negara', '', 'SMAN 1', 'Negeri', 'Ayah', '12', 'Islam', 'D3/S1', 'Karyawan', 'Rohina', '40', 'Islam', 'D3/S1', 'Karyawan', 'riau', '2022-03-30 13:47:35', '2022-03-27 16:03:50'),
+(4, 19, 'huhu', '4545', 'hjhjhj', 'II IPA', '2022-04-02', 'Buddha', 'hghgh', '', 'hjhj', 'hhhh', 'yy', '', 'Kristen Protestan', 'S2', 'ihih', 'jhh', '87', 'Kristen Protestan', 'S2', 'hhh', 'gghghg', '2022-04-02 08:14:21', '2022-04-02 08:14:21');
 
 -- --------------------------------------------------------
 
@@ -198,8 +207,16 @@ CREATE TABLE `tb_user` (
 --
 
 INSERT INTO `tb_user` (`id_user`, `username`, `password`, `level`, `last_login`, `created_at`, `updated_at`) VALUES
-(1, 'admin', '$2y$10$2J8487haXMNup5mpAlbSUeUVJzcqYZuGqimoQVvWZ8s22oa.tRpQG', 'siswa', '2022-02-06 01:23:41', '2022-03-23 04:41:15', '2022-03-23 16:52:56'),
-(2, 'guru', '$2y$10$2J8487haXMNup5mpAlbSUeUVJzcqYZuGqimoQVvWZ8s22oa.tRpQG', 'guru', '2022-03-26 02:47:09', '2022-03-26 02:47:09', NULL);
+(1, 'admin', '$2y$10$vctn6Ifhyzl1/SKdEJ7Nc.kiKOkDcGyhbKuimjoVLUq1tiTc7e3xe', 'siswa', '2022-02-06 01:23:41', '2022-03-23 04:41:15', '2022-04-01 02:57:06'),
+(2, 'guru', '$2y$10$.vsjRAqlaRqW9QdttshOnOiW4MA9DqWA9tvAwlR/JghEzQbaRdM6y', 'guru', '2022-03-26 02:47:09', '2022-03-26 02:47:09', '2022-03-31 16:20:17'),
+(9, 'rila c', '$2y$10$Rmc/gAUL8QxiLrpRzlWA1eJSnr88Bh1qOk/ia3Aw/BnwbWH1WkZC6', 'guru', '2022-03-26 14:58:19', '2022-03-26 14:58:19', '2022-03-26 14:58:19'),
+(13, 'ridho', '$2y$10$dOtf4m.0iqWTWNIsTTbZsOxVYXc5q4my1iMjY0Ef4f//.r8Q1AY5y', 'siswa', '2022-03-26 15:08:52', '2022-03-26 15:08:52', '2022-03-26 15:08:52'),
+(14, 'rilab', '$2y$10$zvPmtt5tsvATMsn4.3LCuerpsWoXf55i3ESUhDaS8yE0SJ5Gr12nW', 'guru', '2022-03-27 15:38:15', '2022-03-27 15:38:15', '2022-03-27 15:38:15'),
+(15, 'ridhos', '$2y$10$NytMZPoh.FGuytXrt2K/WuoBfJc9MxLrfuBhJuCiB9S3e7DWJkv.a', 'siswa', '2022-03-27 16:03:50', '2022-03-27 16:03:50', '2022-03-27 16:03:50'),
+(16, 'helo', '$2y$10$dsBCBBfV16428hFfeb9bF.lfomSgkhu0rj5PWiCeWdXkOFeM4qHw.', 'guru', '2022-04-02 07:56:03', '2022-04-02 07:56:03', '2022-04-02 07:56:03'),
+(17, 'hoi', '$2y$10$ftsM2fKJwFmVEPft.oXJ0eA3IJaivdGUQ4pi9sjC6Y3DSseJ64OlC', 'guru', '2022-04-02 08:03:20', '2022-04-02 08:03:20', '2022-04-02 08:03:20'),
+(18, 'tes', '$2y$10$EOQKwUIfihwx6pIX5J3Oq.48u1HQv7UyI95qxz.Mi4lyy1b6wF0cy', 'guru', '2022-04-02 08:12:53', '2022-04-02 08:12:53', '2022-04-02 08:12:53'),
+(19, 'huhu', '$2y$10$F7UR9gC4zaM/gWOvphrOxe4NXDM7NaWm84CGC93W97gKBXgI92CpO', 'siswa', '2022-04-02 08:14:21', '2022-04-02 08:14:21', '2022-04-02 08:14:21');
 
 --
 -- Indexes for dumped tables
@@ -255,7 +272,7 @@ ALTER TABLE `tb_user`
 -- AUTO_INCREMENT for table `tb_guru`
 --
 ALTER TABLE `tb_guru`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tb_hasil`
@@ -285,13 +302,13 @@ ALTER TABLE `tb_pertanyaan`
 -- AUTO_INCREMENT for table `tb_siswa`
 --
 ALTER TABLE `tb_siswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
